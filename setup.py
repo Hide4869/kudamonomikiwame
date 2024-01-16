d30b32b992723ff8d3ce86711c41dc7abef28942
H!dekazu <87521046+Hide4869@users.noreply.github.com>
2024-01-17 02:51:21 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,26 +2,29 @@ import setuptools
 with open("README.md", "r") as fh:
   long_description = fh.read()
 setuptools.setup(
-  name="KudamonoHanbetsu",
-  version="1.0",
+  name="imageComp",
+  version="0.1.0",
   author="Hidekazu",
   author_email="s2122030@stu.musashino-u.ac.jp",
-  description="Determine when fruit is ready to eat",
+  description="A small example package",
   long_description=long_description,
   long_description_content_type="text/markdown",
-  url="https://github.com/Hide4869/kudamonomikiwame",
+  url="https://github.com/Im-not-yoneda/image_compression",
+  project_urls={
+    "Bug Tracker": "https://github.com/Hide4869/kudamonomikiwame"
+  },
   classifiers=[
-  "Programming Language :: Python :: 3",
-  "License :: OSI Approved :: MIT License",
-  "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
   ],
   package_dir={"": "src"},
-  py_modules=['interipport'],
+  py_modules=['imageComp'],
   packages=setuptools.find_packages(where="src"),
-  python_requires=">=3.8",
+  python_requires=">=3.6",
   entry_points = {
       'console_scripts': [
-          'interipport = interipport:main'
+          'imageComp = imageComp:main'
       ]
   },
 ) 
